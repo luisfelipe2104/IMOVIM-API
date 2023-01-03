b5754db5cd9c0a53bc4328d53ee1167a193bd28e
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-01-03 12:27:31 -0300

    hosting database

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,13 +2,19 @@ import mysql from 'mysql2/promise'
 
 const connect = async () => {
     const connection = await mysql.createConnection({
-        host: 'localhost',
+        host: 'bwlxd3avicainricfhd5-mysql.services.clever-cloud.com',
         port: 3306,
-        user: 'root',
-        password: '',
-        database: 'Imovim'
+        user: 'uxhc3zhysqlflsjr',
+        password: 'uyEmB6pzfMpfx1FxJOdt',
+        database: 'bwlxd3avicainricfhd5'
     })
     return connection
 }
 
 export default { connect }
+
+        // host: 'localhost',
+        // port: 3306,
+        // user: 'root',
+        // password: '',
+        // database: 'Imovim'
