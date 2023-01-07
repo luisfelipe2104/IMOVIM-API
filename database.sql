95c9422f5b39c7005e27769e7973f48cde201c6e
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-01-06 22:14:49 -0300

    database

--- database.sql
+++ database.sql
@@ -16,17 +16,20 @@ CREATE TABLE IF NOT EXISTS Users(
     created_at datetime default now()
 );
 
+SELECT * FROM Profile;
 
--- MISTERIO 
--- #CREATE TABLE IF NOT EXISTS Profile(
--- #	id int primary key,
---  #   user_id int not null,
---  #   nickname varchar(30) unique,
---   #  description text,
---    # localization varchar(100),
+CREATE TABLE IF NOT EXISTS Profile(
+	id int primary key auto_increment,
+    user_id int not null,
+    profileImage varchar(255),
+    profileBackground varchar(255),
+    description text,
+    localization varchar(100),
     
---     #constraint user_ID_tblProfile foreign key(user_id) references Users(id)
--- #);
+    constraint user_ID_tblProfile foreign key(user_id) references Users(id)
+);
+
+select * from Profile;
 
 CREATE TABLE IF NOT EXISTS UserFollowing(
 	id int primary key auto_increment,
@@ -44,6 +47,7 @@ CREATE TABLE IF NOT EXISTS Posts(
 	caption text,
 	created_at datetime default now() not null,
 	image varchar(255),
+    updated boolean default false,
     
     constraint user_ID_tblPosts foreign key(user_id) references Users(id)
 );
@@ -57,12 +61,38 @@ CREATE TABLE IF NOT EXISTS UserLikesPost(
     constraint user_ID_tblUserLikes foreign key(user_id) references Users(id),
     constraint post_ID_tblUserLikes foreign key(post_id) references Posts(id)
 );
--- SELECT count(*) followers FROM UserFollowing WHERE user_id = 3;
 
--- SELECT * FROM UserFollowing;
+SELECT comment, created_at, (SELECT nickname FROM Users WHERE id = c.user_id) AS nickname, (SELECT profileImage FROM Profile p WHERE p.user_id = c.user_id) profileImage FROM Comments c WHERE post_id = 1;
 
--- SELECT follower.nickname FROM UserFollowing t JOIN Users follower ON t.follower_id = follower.id WHERE user_id = 3 ORDER BY follower.nickname ASC;
+CREATE TABLE IF NOT EXISTS Comments(
+	id int primary key auto_increment,
+    post_id int not null,
+    user_id int not null,
+    comment text not null,
+    created_at datetime default now(),
+    updated boolean default false,
+    
+    constraint post_id_tblComments foreign key(post_id) references Posts(id),
+    constraint user_id_tblComments foreign key(user_id) references Users(id)
+);
 
--- SELECT nickname, caption, image, p.created_at, (SELECT COUNT(*) FROM UserLikesPost WHERE post_id = p.id) AS likes FROM Posts p JOIN Users u ON u.id = user_id WHERE user_id IN (SELECT user_id FROM UserFollowing WHERE follower_id = 5) ORDER BY likes DESC;
 
--- SELECT nickname, caption, image, p.created_at, (SELECT COUNT(*) FROM UserLikesPost WHERE post_id = p.id) AS likes FROM Posts p JOIN Users u ON u.id = user_id; 
+SELECT count(*) followers FROM UserFollowing WHERE user_id = 3;
+
+SELECT * FROM Comments;
+
+INSERT INTO Posts(user_id, caption, image) VALUES(1, "teste", "image");
+
+
+
+
+
+SELECT * FROM Posts;
+
+SELECT * FROM UserFollowing;
+
+SELECT follower.nickname FROM UserFollowing t JOIN Users follower ON t.follower_id = follower.id WHERE user_id = 3 ORDER BY follower.nickname ASC;
+
+SELECT nickname, caption, image, p.created_at, (SELECT COUNT(*) FROM UserLikesPost WHERE post_id = p.id) AS likes FROM Posts p JOIN Users u ON u.id = user_id WHERE user_id IN (SELECT user_id FROM UserFollowing WHERE follower_id = 5) ORDER BY likes DESC;
+
+SELECT nickname, caption, image, p.created_at, (SELECT COUNT(*) FROM UserLikesPost WHERE post_id = p.id) AS likes FROM Posts p JOIN Users u ON u.id = user_id; 
