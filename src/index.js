66af55543ce13fae199fbf3e880e1056b173e0af
Alunos <aluno.etec@etecEmbu.com.br>
2023-03-23 22:03:22 -0300

    Adding compression

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import compression from 'compression'
 import express from 'express'
 import cors from 'cors'
 import routes from './routes.js'
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 3333
 
 app.use(express.json())
 app.use(cors())
+app.use(compression())
 app.use("/", routes)
 
 app.listen(PORT, () => {
