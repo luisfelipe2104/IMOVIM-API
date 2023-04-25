718a535f6c317f2584b6bc225ae71bf27324ee5d
luisfelipe2104 <gomesluisfelipe05@gmail.com>
2023-04-25 20:32:07 -0300

    fixing nodemailer bugs

--- src/mailer/sendMail.js
+++ src/mailer/sendMail.js
@@ -1,12 +1,26 @@
-import { createTransporter } from "./config.js";
+import nodemailer from "nodemailer";
+import { config } from "dotenv";
 
-export async function sendMailText(to, subject, text) {
-    const mailOptions = {
+config()
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PWD_APP
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  })
+
+  export const sendMailText = async (to, subject, text) => {
+      transporter.sendMail({
         from: process.env.EMAIL_USER,
         to,
         subject,
         text
-    }
-
-    await createTransporter().sendMail(mailOptions)
-}
+      })
+  } 
