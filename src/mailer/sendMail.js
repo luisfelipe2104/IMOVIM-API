fb15b41e1aa470c0ebf16b38027bf21f042984da
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-04-26 14:45:11 -0300

    changing email port and security

--- src/mailer/sendMail.js
+++ src/mailer/sendMail.js
@@ -5,8 +5,8 @@ config()
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PWD_APP
