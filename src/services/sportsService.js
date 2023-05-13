bea0ebb8b5864b8a5268850caf4417a7b96884a1
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-05-12 23:17:36 -0300

    organizing code

--- src/services/sportsService.js
+++ src/services/sportsService.js
@@ -2,44 +2,51 @@ import db from "../database/connection.js";
 import { SportView } from "../../database/views.js";
 
 async function getSports() {
-    const conn = await db.connect()
-    const sql = 'SELECT * FROM Sports'
-    const results = await conn.query(sql)
-    conn.end()
-    return results[0]
+  const conn = await db.connect();
+  const sql = "SELECT * FROM Sports";
+  const results = await conn.query(sql);
+  conn.end();
+  return results[0];
 }
 
 async function checkUserPracticesSport(user_id, sport_id) {
-    const conn = await db.connect()
-    const sql = `${SportView} WHERE user_id = ? AND sport_id = ?`
-    const data = [user_id, sport_id]
-    const results = await conn.query(sql, data)
-    conn.end()
-    return results[0]
+  const conn = await db.connect();
+  const sql = `${SportView} WHERE user_id = ? AND sport_id = ?`;
+  const data = [user_id, sport_id];
+  const results = await conn.query(sql, data);
+  conn.end();
+  return results[0];
 }
 
 async function insertSportPracticed(user_id, sport_id) {
-    const conn = await db.connect()
-    const sql = 'INSERT INTO UserPracticeSport(user_id, sport_id) VALUES (?, ?)'
-    const data = [user_id, sport_id]
-    await conn.query(sql, data)
-    conn.end()
+  const conn = await db.connect();
+  const sql = "INSERT INTO UserPracticeSport(user_id, sport_id) VALUES (?, ?)";
+  const data = [user_id, sport_id];
+  await conn.query(sql, data);
+  conn.end();
 }
 
 async function deleteSportPracticed(user_id, sport_id) {
-    const conn = await db.connect()
-    const sql = 'DELETE FROM UserPracticeSport WHERE user_id = ? AND sport_id = ?'
-    const data = [user_id, sport_id]
-    await conn.query(sql, data)
-    conn.end()
+  const conn = await db.connect();
+  const sql =
+    "DELETE FROM UserPracticeSport WHERE user_id = ? AND sport_id = ?";
+  const data = [user_id, sport_id];
+  await conn.query(sql, data);
+  conn.end();
 }
 
 export async function getSportsPracticed(user_id) {
-    const conn = await db.connect()
-    const sql = `${SportView} WHERE user_id = ?`
-    const results = await conn.query(sql, [user_id])
-    conn.end()
-    return results[0]
+  const conn = await db.connect();
+  const sql = `${SportView} WHERE user_id = ?`;
+  const results = await conn.query(sql, [user_id]);
+  conn.end();
+  return results[0];
 }
 
-export default { getSports, getSportsPracticed, insertSportPracticed, checkUserPracticesSport, deleteSportPracticed }
+export default {
+  getSports,
+  getSportsPracticed,
+  insertSportPracticed,
+  checkUserPracticesSport,
+  deleteSportPracticed,
+};
